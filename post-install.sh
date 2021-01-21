f742f9c601fa952137828ec942447371f51ec79c
Pierre-Alexis <pierre-alexis.blond@live.fr>
2021-01-21 13:29:20 +0100

    create gnome install

--- post-install.sh
+++ post-install.sh
@@ -15,9 +15,8 @@ pacman -Syu
 
 echo "****************"
 echo "KDE installation"
-pacman -S xorg xorg-{xinit,server} sddm plasma plasma-nm
-systemctl disable gdm
-systemctl enable sddm
+pacman -S xorg xorg-server gnome
+systemctl enable gdm.service
 systemctl enable NetworkManager
 
 echo "*****************"
@@ -32,56 +31,3 @@ pacman -S sudo
 useradd -m -g wheel -s /bin/bash login
 passwd login
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
- 
-pacman -S firefox ark vlc qt4 dolphin git code konsole libreoffice-still python-virtualenv
-
-echo "*****************************"
-echo "Java environment installation"
-pacman -S jre8-openjdk
-
-echo "***************************"
-echo "Nvidia drivers installation"
-pacman -S nvidia nvidia-settings
-
-echo "*****************"
-echo "Node installation"
-pacman -S nodejs npm
-npm i -g n yarn
-n lts
-
-echo "**********************************"
-echo "Docker/Docker-compose installation"
-pacman -S docker docker-compose
-systemctl enable docker
-groupadd docker
-usermod -aG docker login
-
-echo "***********************"
-echo "Latte Dock installation"
-pacman -S git cmake extra-cmake-modules python plasma-framework plasma-desktop
-git clone https://github.com/KDE/latte-dock.git
-cd latte-dock
-chmod +x install.sh
-./install.sh
-cd ..
-rm -rf latte-dock
-
-echo "**************************"
-echo "Infosec tools installation"
-pacman -S nmap tcpdump wireshark-qt openssh openvpn sqlmap
-mkdir /home/login/sec
-mkdir /home/login/sec/tools
-cd /home/login/sec/tools
-git clone https://github.com/PABlond/Kali-container-for-pentest.git kali
-cd kali
-docker-compose up -d
-cd $USER
-
-echo "**************************"
-echo "Google Chrome installation"
-git clone https://aur.archlinux.org/google-chrome.git
-cd google-chrome
-runuser -l login -c "makepkg -s"
-pacman -U google-chrome-*.tar.xz
-cd ..
-rm -rf google-chrome
