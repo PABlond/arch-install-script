b868e7540bea11a4033f4b9214a10dee6db5fcd9
PABlond <pierre-alexis.blond@live.fr>
2024-04-14 19:29:54 +0200

    add neovim config

---
+++ .config/nvim/lua/enzom/plugins.lua
@@ -0,0 +1,137 @@
+local plugins = {
+
+	-- Folke's good stuff
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{ "folke/trouble.nvim" },
+	{ "folke/zen-mode.nvim" },
+	-- End of Folke's good stuff
+
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"folke/todo-comments.nvim",
+		opts = {},
+	},
+	{ "williamboman/mason.nvim" },
+	{ "nvimdev/lspsaga.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "onsails/lspkind.nvim" },
+	{ "ThePrimeagen/harpoon", name = "harpoon" },
+	{ "nvimtools/none-ls.nvim" },
+
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "mrjones2014/smart-splits.nvim", lazy = true },
+	{ "kylechui/nvim-surround" },
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = "markdown",
+		-- build = "cd app && yarn install",
+		build = ":call mkdp#util#install()",
+	},
+	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		defaults = {
+			lazy = false,
+			version = nil,
+		},
+		build = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"David-Kunz/treesitter-unit",
+			"windwp/nvim-ts-autotag",
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"sindrets/diffview.nvim", -- optional - Diff integration
+		},
+		config = true,
+	},
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "windwp/nvim-autopairs" },
+        {
+            "mhartington/formatter.nvim",
+            config = function()
+                local formatter_prettier = {require("formatter.defaults.prettier")}
+                require("formatter").setup(
+                    {
+                        filetype = {
+                            javascript = formatter_prettier,
+                            javascriptreact = formatter_prettier,
+                            typescript = formatter_prettier,
+                            typescriptreact = formatter_prettier
+                        }
+                    }
+                )
+                -- automatically format buffer before writing to disk:
+                vim.api.nvim_create_augroup("BufWritePreFormatter", {})
+                vim.api.nvim_create_autocmd(
+                    "BufWritePre",
+                    {
+                        command = "FormatWrite",
+                        group = "BufWritePreFormatter",
+                        pattern = {"*.js", "*.jsx", "*.ts", "*.tsx"}
+                    }
+                )
+            end,
+            ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"}
+        },
+        ---- indendation detection
+        -- automatically configure indentation when a file is opened.
+
+        {"nmac427/guess-indent.nvim"},
+        {
+            'dense-analysis/ale',
+            config = function()
+                -- Configuration goes here.
+                local g = vim.g
+
+                g.ale_ruby_rubocop_auto_correct_all = 1
+
+                g.ale_linters = {
+                    typescript = {'prettier', 'eslint'},
+                    typescriptreact = {'prettier', 'eslint'},
+                    css = {'prettier'},
+                    json = {'prettier'},
+                    javascript = {'prettier', 'eslint'},
+                    ruby = {'rubocop', 'ruby'},
+                    lua = {'lua_language_server'}
+                }
+            end
+        }
+}
+
+require("lazy").setup(plugins, opts)
