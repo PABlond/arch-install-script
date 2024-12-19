1bd2aa8b92ee43ce617c0bf3b304cf46845472c6
PABlond <pierre.alexis.blond@gmail.com>
2024-12-19 13:30:48 +0100

    update nvim config

--- .config/nvim/nvim/lua/custom/plugins/buffers.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'nvim-lua/plenary.nvim',
-  'j-morano/buffer_manager.nvim',
-}
--- .config/nvim/nvim/lua/custom/plugins/typescript.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'pmizio/typescript-tools.nvim',
-    requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    config = function()
-      require('typescript-tools').setup {}
-    end,
-  },
-}
