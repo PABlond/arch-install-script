babe6913169b64ffba4849af12266a256b3472f9
PABlond <pierre.alexis.blond@gmail.com>
2025-03-29 13:41:42 +0100

    update config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 vim.cmd.colorscheme "cyberdream"
 -- Oil 
+vim.keymap.set("n", "=", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- Buffer management
 vim.keymap.set("n", "<leader>=", ':lua require("buffer_manager.ui").toggle_quick_menu()<CR>')
@@ -22,6 +23,14 @@ vim.keymap.set("n", "<leader>q", ":bw!<CR>")
 vim.keymap.set("n", "<leader>sf", ":FzfLua grep<CR>")
 
 vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"  -- Active le presse-papiers système
+
+-- Raccourcis pour copier/coller/couper/sélectionner
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true, desc = "Copy to system clipboard" })
+vim.keymap.set("n", "<C-v>", '"+p', { noremap = true, silent = true, desc = "Paste from system clipboard" })
+vim.keymap.set("i", "<C-v>", '<C-r>+', { noremap = true, silent = true, desc = "Paste from system clipboard" })
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all text" })
+vim.keymap.set("v", "<C-x>", '"+x', { noremap = true, silent = true, desc = "Cut to system clipboard" })
 
 vim.api.nvim_set_keymap(
   "i", 
@@ -37,3 +46,23 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true }
 )
 
+
+-- Fonction pour obtenir le chemin relatif et le copier
+local function copy_relative_path()
+  -- Récupère le chemin absolu du fichier courant
+  local absolute_path = vim.fn.expand('%:p')
+  -- Récupère le répertoire de travail (par exemple ~/.dev/monprojet)
+  local project_root = vim.fn.getcwd()
+  -- Calcule le chemin relatif
+  local relative_path = vim.fn.substitute(absolute_path, project_root .. '/', '', '')
+  -- Copie dans le presse-papiers (utilise le registre '+' pour le presse-papiers système)
+  vim.fn.setreg('+', relative_path)
+  -- Message de confirmation
+  print('Chemin relatif copié : ' .. relative_path)
+end
+
+-- Associe la fonction à une commande utilisateur
+vim.api.nvim_create_user_command('CopyRelPath', copy_relative_path, {})
+
+-- Mappe la combinaison '@.' à cette commande
+vim.keymap.set('n', '@.', ':CopyRelPath<CR>', { noremap = true, silent = true })
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -2,4 +2,15 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+        require("cyberdream").setup({
+            -- Options de configuration ici
+            transparent = true,
+            highlights = {
+        LineNr = { fg = "#FFFFFF" }, -- Couleur des numéros de ligne
+        NormalFloat = { bg = "NONE" }, -- Fenêtres flottantes transparentes
+    },
+        })
+        vim.cmd("colorscheme cyberdream")
+    end,
 }
